370886b7f23ad9e999ae8d56f60ce98fd6169e0c
Nick Kinlen <nick_nyhc88@hotmail.com>
2019-01-24 20:39:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Iterates from 1-100
 for(var i = 1; i <= 100; i++) {
-	// Checks if number is divisible by two and three
+  // Checks if number is divisible by two and three
   if(i % 2 == 0 && i % 3 == 0) {
   	console.log('The number ' + i + ' is divisible by two and three');
   }
